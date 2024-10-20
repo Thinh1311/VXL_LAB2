d72448c24c0205987f3554024e16341d63e3dd4b
= <=>
2024-10-21 01:06:43 +0700

    Update code

--- codeSTM32/Core/Src/main.c
+++ codeSTM32/Core/Src/main.c
@@ -231,28 +231,28 @@ int led_buffer[4] = {1, 2, 3, 4};
 void update7SEG(int index){
 	switch(index){
 	case 0:
-		display7SEG(led_buffer[index]);
+		display7SEG(led_buffer[0]);
 		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
 		break;
 	case 1:
-		display7SEG(led_buffer[index]);
+		display7SEG(led_buffer[1]);
 		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
 		break;
 	case 2:
-		display7SEG(led_buffer[index]);
+		display7SEG(led_buffer[2]);
 		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
 		HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
 		break;
 	case 3:
-		display7SEG(led_buffer[index]);
+		display7SEG(led_buffer[3]);
 		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
 		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
 		HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
